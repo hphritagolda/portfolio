ec706f98c7e44aa2476c5431f7199dc61eacf412
fezproof <26830309+fezproof@users.noreply.github.com>
2022-07-20 18:58:05 +0800

    Added better icon support Added web manifest

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,24 @@ import Header from "./components/Header";
 
 import styles from "./styles/tailwind.css";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  { rel: "manifest", href: "/site.webmanifest" },
+  { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -20,6 +37,9 @@ export const meta: MetaFunction = () => ({
   title: "Hanna Pritagolda",
   description:
     "A digital design student, an occassional youtuber & a serious foodie.",
+
+  "msapplication-TileColor": "#fffbf6",
+  "theme-color": "#fffbf6",
 });
 
 export function CatchBoundary() {
